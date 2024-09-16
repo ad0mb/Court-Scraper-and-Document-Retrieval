04ac19a1d0efdc6cf86664fc313640970084ccf4
ad0m_b <abouloudene@outlook.com>
2024-09-16 18:04:51 -0400

    fileDownloaded Check addition to code
    -Added fileDownloadedCheck with an input name parameter to check if a file was downloaded from the retrieval portion of the flow.

--- src/Projects/CourtScraper/Helpers/CheckIfRetrieved.java
+++ src/Projects/CourtScraper/Helpers/CheckIfRetrieved.java
@@ -1,10 +1,12 @@
 package CourtScraper.Helpers;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import static CourtScraper.Helpers.FileManagement.tempDownloadsFolderPath;
 import static CourtScraper.Setups.GUI.MainPanelElements.MainComboBoxes.selectedCounty;
 import static CourtScraper.Setups.GUI.MainPanelElements.MainComboBoxes.selectedState;
 
@@ -25,5 +27,19 @@ public class CheckIfRetrieved {
             }
         }
         return false;
+    }
+
+    public static boolean fileDownloadedCheck(String fileName) {
+        File tempFolder = new File(tempDownloadsFolderPath + "\\" + fileName + ".pdf");
+        //sets infolder to false for loop
+        boolean inFolder = false;
+
+        while (!inFolder) {
+            //if the folder path containing the given parameter name exists it will mark true to end the loop
+            if (tempFolder.exists()) {
+                inFolder = true;
+            }
+        }
+        return inFolder;
     }
 }
