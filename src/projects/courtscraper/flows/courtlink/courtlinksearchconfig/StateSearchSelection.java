78cdcdbe46067add8a7afb1c485925d9f02b4a35
ad0m_b <abouloudene@outlook.com>
2024-10-07 11:26:15 -0400

    Cleaned up Courtlink state search configuration code.

--- src/projects/courtscraper/flows/courtlink/courtlinksearchconfig/StateSearchSelection.java
+++ src/projects/courtscraper/flows/courtlink/courtlinksearchconfig/StateSearchSelection.java
@@ -14,35 +14,20 @@ public class StateSearchSelection extends CourtlinkSearchConfigMain {
     //this is the dropdown selection portion of the courtlink search flow
 
     public static void searchState(String state) throws InterruptedException {
+        String inputString = "";
         switch (state) {
-            case "California":
-                driver.findElement(By.xpath("//*[@id=\"courtsdropdown\"]")).sendKeys("California " + processedInputs[5]);
-                driver.findElement(By.xpath("/html/body/main/div/ln-courtlinksearchform/div/searchform/div[1]/div[1]/courtlist/div/courtlistselector/div/div[2]/div/div[2]/div/div/div/div/label/div")).click();
-                break;
             case "Florida":
-                driver.findElement(By.xpath("//*[@id=\"courtsdropdown\"]")).sendKeys("Florida " + processedInputs[5]);
-                driver.findElement(By.xpath("/html/body/main/div/ln-courtlinksearchform/div/searchform/div[1]/div[1]/courtlist/div/courtlistselector/div/div[2]/div/div[2]/div/div/div/div/label/div")).click();
-                break;
-            case "New York":
-                if (processedInputs[5].equals("New York BKQ")) {
-                    String[] BKQ = {"Bronx", "Kings", "Queens"};
-                    for (String burrough : BKQ) {
-                        driver.findElement(By.xpath("//*[@id=\"courtsdropdown\"]")).sendKeys("New York " + burrough);
-                        driver.findElement(By.xpath("/html/body/main/div/ln-courtlinksearchform/div/searchform/div[1]/div[1]/courtlist/div/courtlistselector/div/div[2]/div/div[2]/div/div/div/div/label/div")).click();
-                        driver.findElement(By.xpath("//*[@id=\"courtsdropdown\"]")).clear();
-                    }
-                } else {
-                    driver.findElement(By.xpath("//*[@id=\"courtsdropdown\"]")).sendKeys("New York " + processedInputs[5]);
-                    driver.findElement(By.xpath("/html/body/main/div/ln-courtlinksearchform/div/searchform/div[1]/div[1]/courtlist/div/courtlistselector/div/div[2]/div/div[2]/div/div/div/div/label/div")).click();
-                }
+                inputString = state + " " + processedInputs[5];
                 break;
             case "Texas":
-                driver.findElement(By.xpath("//*[@id=\"courtsdropdown\"]")).sendKeys("Texas " + processedInputs[5] + " district");
-                Thread.sleep(1000);
-                driver.findElement(By.xpath("/html/body/main/div/ln-courtlinksearchform/div/searchform/div[1]/div[1]/courtlist/div/courtlistselector/div/div[2]/div/div[2]/div/div/div/div/label/div")).click();
-                Thread.sleep(1000);
+                inputString = state + " " + processedInputs[5] + " district";
                 break;
         }
+
+        driver.findElement(By.xpath("//*[@id=\"courtsdropdown\"]")).sendKeys(inputString);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/main/div/ln-courtlinksearchform/div/searchform/div[1]/div[1]/courtlist/div/courtlistselector/div/div[2]/div/div[2]/div/div/div/div/label/div")).click();
+        Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/main/div/ln-courtlinksearchform/div/searchform/div[1]/segmentcontrols/div[1]/div[1]/div[2]/input")).click();
     }
 }
